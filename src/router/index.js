9d1c4d774b85d7e78f193f7c4652463a3da8aba0
exca <excamuchlisandita@gmail.com>
2023-02-20 21:44:48 +0700

    set up bottom-tab-navigator

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import {createStackNavigator} from '@react-navigation/stack';
-import {GetStarted, Register, Splash, Login, UploadPhoto} from "../pages";
+import {GetStarted, Register, Splash, Login, UploadPhoto, Doctor, Hospitals, Messages} from "../pages";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const MainApp
+const MainApp = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Doctor" component={Doctor}/>
+            <Tab.Screen name="Message" component={Messages}/>
+            <Tab.Screen name="Hospital" component={Hospitals}/>
+        </Tab.Navigator>
+    )
+}
 
 const Router = () => {
     return (
@@ -16,6 +24,7 @@ const Router = () => {
             <Stack.Screen name="Register" component={Register} options={{headerShown: false}}/>
             <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
             <Stack.Screen name="UploadPhoto" component={UploadPhoto} options={{headerShown: false}}/>
+            <Stack.Screen name="MainApp" component={MainApp} options={{headerShown: false}}/>
         </Stack.Navigator>
     );
 };
